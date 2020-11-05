3a14763fbb15ab1cb6c4188530e3f4e58febfff9
Loran <laurent.muller@bepatient.com>
2020-11-05 11:00:50 +0100

    4-resolve: Added route resolver

--- www/js/todos/todos.config.js
+++ www/js/todos/todos.config.js
@@ -7,8 +7,15 @@
         templateUrl: "./js/todos/todos.component.html",
         controller: "todosController",
         controllerAs: "vm",
+        resolve: {
+          todos: getTodos,
+        },
       },
     ];
+
+    function getTodos(TodosService) {
+      return TodosService.getTodos();
+    }
 
     routes.forEach(function (route) {
       $stateProvider.state(route);
--- www/js/todos/todos.controller.js
+++ www/js/todos/todos.controller.js
@@ -1,26 +1,15 @@
 (function () {
   "use strict";
-  function controller(TodosService) {
+
+  function controller(todos) {
     var ctrl = this;
+    ctrl.todos = todos;
     ctrl.title = "Mes trucs a faire";
-    ctrl.todos = [];
 
     ctrl.shouldShowDelete = false;
     ctrl.shouldShowReorder = false;
     ctrl.listCanSwipe = true;
-
-    init();
-    
-
-    function getTodos() {
-      TodosService.getTodos().then(function (todos) {
-        ctrl.todos = todos;
-      });
-    }
-
-    function init() {
-      getTodos();
-    }
   }
+
   angular.module("todosModule").controller("todosController", controller);
 })();
